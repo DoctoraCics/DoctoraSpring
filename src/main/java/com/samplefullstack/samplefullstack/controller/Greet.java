980f5a2d73c631300540cccb4d150cda26987a23
Feivel Doctora <feivellixen.doctora.iics@ust.edu.ph>
2021-11-14 03:06:48 +0800

    - Reset

---
+++ src/main/java/com/samplefullstack/samplefullstack/controller/Greet.java
@@ -0,0 +1,21 @@
+package com.samplefullstack.samplefullstack.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController("/api/v1/greet")
+public class Greet
+{
+    @RequestMapping("/hello")
+    public String hello()
+    {
+        return "hello world";
+
+    }
+    @RequestMapping("/hi")
+    public String hi()
+    {
+        return "hi";
+    }
+}
