c5dc7e7ecdf4831f43ebb3b68adcd2c9a0defee8
Feivel Doctora <feivellixen.doctora.iics@ust.edu.ph>
2021-11-19 11:55:21 +0800

    - Implemented API - SQL interaction

--- src/main/java/com/samplefullstack/samplefullstack/controller/Greet.java
+++ src/main/java/com/samplefullstack/samplefullstack/controller/Greet.java
@@ -1,17 +1,19 @@
 package com.samplefullstack.samplefullstack.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
-@RestController("/api/v1/greet")
+
+@RestController
+@RequestMapping("/api/v1/greet")
+@CrossOrigin("*") //allow all connections
 public class Greet
 {
     @RequestMapping("/hello")
     public String hello()
     {
         return "hello world";
-
     }
     @RequestMapping("/hi")
     public String hi()
